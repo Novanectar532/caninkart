9fc4d2fc752797fdebf1bb56141c129262bd80b7
Santosh Kumar <sk8065788@gmail.com>
2025-06-24 14:06:16 +0530

    changes in the viteconfigfile

--- CaninKart/vite.config.js
+++ CaninKart/vite.config.js
@@ -1,13 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+//   build: {
+//     assetsInlineLimit: 0,
+//   },
+
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   build: {
-    assetsInlineLimit: 0,
-  },
-
+    assetsInlineLimit: 0,
+    minify: false, // prevent extra CPU use on minification
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  assetsInclude: ['/.png', '/.jpg', '/.jpeg', '/.svg'],
 })
